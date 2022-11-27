b41f6b8617206321727aa43f064f42fd4564b593
EnrymsNTT303 <avicii20.06@gmail.com>
2022-11-28 06:43:13 +0700

    update 3

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           </label>
           <input type="text" name="username" placeholder="Username" id="username" required>
           <label for="password">
-              <i class="fas fa-lock"></i>
+              <i class="fa-light fa-lock"></i>
           </label>
           <input type="password" name="password" placeholder="Password" id="password" required>
           <input type="submit" value="Login">
